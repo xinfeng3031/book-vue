d59ac7787b499f6a7496bde414aa846b430bd071
Leo <xinfeng3031@163.com>
2017-11-28 15:09:26 +0800

    init vue project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <h1>Hellow World!</h1>
+        <p>{{test}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+    name: 'hello world',
+        data () {
+            return {
+                test: '开始学习vue'
+            }
+        }
+    }
+</script>
